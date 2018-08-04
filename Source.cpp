7fa9f7b496227442f5b2805d5979045a00c5b4ff
pyongie210 <41872735+pyongie210@users.noreply.github.com>
2018-08-04 12:44:17 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	auto start = std::chrono::high_resolution_clock::now();
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff = end - start; //double, duration
+	cout << "Time " << diff.count()  << "\n";
+// chrono now, chrono 
+	system("pause");
+	return 0;
+}
