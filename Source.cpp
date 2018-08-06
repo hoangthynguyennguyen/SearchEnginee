6ae3d3655f2924aa1c96bd4c8299f3b13140a129
pyongie210 <41872735+pyongie210@users.noreply.github.com>
2018-08-06 20:15:02 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -8,6 +8,7 @@ int main()
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 	auto start = std::chrono::high_resolution_clock::now();
+	// declare variables and function here
 	auto end = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> diff = end - start; //double, duration
 	cout << "Time " << diff.count()  << "\n";
